60bdc91d5a43543f97d397f00fe4f66ff4201bda
earlkian8 <earlkian8@gmail.com>
2025-10-16 00:29:13 +0800

    fix: map

--- src/components/EventDetails/map.jsx
+++ src/components/EventDetails/map.jsx
@@ -1,23 +1,14 @@
-import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import L from 'leaflet';
-import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
-import markerIcon from 'leaflet/dist/images/marker-icon.png';
-import markerShadow from 'leaflet/dist/images/marker-shadow.png';
-
-delete L.Icon.Default.prototype._getIconUrl;
-L.Icon.Default.mergeOptions({
-    iconRetinaUrl: markerIcon2x,
-    iconUrl: markerIcon,
-    shadowUrl: markerShadow,
-});
 
 const Map = ({ lat, lon, venue }) => {
     const center = [Number(lat) || 0, Number(lon) || 0];
 
     return (
         <MapContainer center={center} zoom={15} style={{ width: '100%', height: '100%' }}>
-            <TileLayer/>
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
             <Marker position={center}>
                 <Popup>
                     <div className="font-semibold">{venue?.name}</div>
